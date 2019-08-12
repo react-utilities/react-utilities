49859222c4505b310eaac400a3f07227533f501f
Jeff Wainwright <yowainwright@gmail.com>
2019-08-12 11:57:01 -0700

    init adds working copied monorepo ts

---
+++ src/react-element-width-detector/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'react-element-width-detector',
+  testEnvironment: 'node',
+  resetMocks: true,
+  rootDir: './',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  }
+};
---
+++ src/react-vertical-scroll-distance-detector/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'react-vertical-scroll-distance-detector',
+  testEnvironment: 'node',
+  resetMocks: true,
+  rootDir: './',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  }
+};
