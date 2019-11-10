5663369dec9ca512d42a7d14817874e51d9ed12e
Jeff Wainwright <yowainwright@gmail.com>
2019-11-10 14:52:56 -0800

    fix(tests): more tests fixes; still errors

--- packages/react-vertical-scroll-distance-detector/src/__tests__/index.test.tsx
+++ packages/react-vertical-scroll-distance-detector/src/__tests__/index.test.tsx
@@ -1,12 +1,15 @@
-import { VerticalScrollDistanceProvider, VerticalScrollDistanceConsumer,  } from '../';
+import {
+  VerticalScrollDistanceProvider,
+  VerticalScrollDistanceConsumer
+} from '..'
 // import { MockScrollDistanceImplementation } from '../Mock'
 
 describe('VerticalScrollDistanceDetector', () => {
   it('VerticalScrollDistanceProvider', () => {
-    expect(typeof VerticalScrollDistanceProvider).toBe('function');
-  });
+    expect(typeof VerticalScrollDistanceProvider).toBe('function')
+  })
 
   it('VerticalScrollDistanceConsumer', () => {
-    expect(typeof VerticalScrollDistanceConsumer).toBe('function');
-  });
-});
+    expect(typeof VerticalScrollDistanceConsumer).toBe('function')
+  })
+})
