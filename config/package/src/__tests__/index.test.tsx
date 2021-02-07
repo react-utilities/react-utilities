ecc468c497c744f0c55b589cfa88526a7eb8faa7
yowainwright <yowainwright@gmail.com>
2021-02-07 01:55:40 -0800

    feat(root): init new package cli

---
+++ config/package/src/__tests__/index.test.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { Foo } from '../index'
+
+describe('<package>', () => {
+  test('Foo', () => {})
+})
---
+++ config/package/src/index.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react'
+
+export const Foo = (): JSX.Element => <div>Bar</div>
